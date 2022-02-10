fb856e4067f0e96e30bbdac271caf35b493ad3b6
moritzviolin <95185309+moritzviolin@users.noreply.github.com>
2022-02-10 23:12:01 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use termion::color;
 use std::io;
-
+use std::{thread, time};
 
 mod wordlist;
 
@@ -18,15 +18,14 @@ fn print_manual() {
     println!("https://github.com/tabatkins/wordle-list");
     println!("-------------------------------------------------------------------------");
     println!("");
+    println!("");
+    println!("LET THE GUESSING BEGIN!");
+    println!("");
 }
 
 
-fn generate_word() -> [char; 5] {
-    let answer = wordlist_actions::random_word();
-    //println!("{}", answer);
-
+fn generate_wordlist(answer: &str) -> [char; 5] {
     let mut answered_character_list: [char; 5] = ['a','a','a','a','a'];
-
     for i in 0.. 5 {
         //put chars of the solution into a list for easier comparison during guess_validation()
         let answered_character = answer.chars().nth(i).unwrap();
@@ -49,16 +48,17 @@ fn guessing(guess_nr: i32) -> [char; 5] {
     let reader = io::stdin();
 
     loop {
+        //input loop to guarantee that each guess has 5 chars
         let mut guess: String = String::new();
         println!("Enter your {} guess:", guess_number);
         let _result = reader.read_line(&mut guess);
+        //remove the newline character added by pressing enter
         guess.pop();
-        println!("len: {}", guess.len());
+        //println!("len: {}", guess.len()); //<-- comment out for actual build, useful for debugging
         if guess.len() == 5 {
-            println!("You guessed: {}", guess);
-
+            //validate guess length -> proceed
+            //println!("You guessed: {}", guess); //<-- comment out for actual build, useful for debugging
             let mut guessed_character_list: [char; 5] = ['a','a','a','a','a'];
-
             for i in 0.. 5 {
                 //put chars of the guessed word into a list for easier comparison during guess_validation() and easier printout of the validated guess
                 let guessed_character = guess.chars().nth(i).unwrap();
@@ -74,24 +74,40 @@ fn guessing(guess_nr: i32) -> [char; 5] {
 
 
 fn guess_validation(guessed_character_list: [char; 5], answered_character_list: [char; 5]) -> Vec<&'static str> {
+    let mut guess_working_list = guessed_character_list;
+    let mut answer_working_list = answered_character_list;
 
     let mut coloring = Vec::new();
+    let mut green_char_indices = Vec::new();
+    let mut red_char_indices = Vec::new();
 
-    //validate the characters (right place, in word, not in word)
+    //look for all right characters
     for pos in 0..5 {
-        //check, if in right place and recolor
-        if guessed_character_list[pos] == answered_character_list[pos] {
-            coloring.push("green");
+        if guess_working_list[pos] == answer_working_list[pos] {
+            green_char_indices.push(pos);
+            guess_working_list[pos] = '0';
+            answer_working_list[pos] = '0';
         }
-        else {
-            //check if in word and recolor
-            if char_in_list(answered_character_list, guessed_character_list[pos]) {
-                coloring.push("red");
-            }
-            else {
-                coloring.push("black");
-            }
+    }
+    //look for all misplaced characters
+    for pos in 0..5 {
+        if char_in_list(answer_working_list, guess_working_list[pos]) {
+            red_char_indices.push(pos);
+            //removing the characters from the list so multiple occurances of the same character won't cause problems
+            guess_working_list[pos] = '0';
+            answer_working_list[pos] = '0';
         }
+    }
+    //setting black characters as a default value
+    for _i in 0..5 {
+        coloring.push("black")
+    }
+    //overwriting the colors to match the state of the letter
+    for pos in red_char_indices {
+        coloring[pos] = "red";
+    }
+    for pos in green_char_indices {
+        coloring[pos] = "green";
     }
     return coloring
 }
@@ -108,23 +124,33 @@ fn char_in_list(list: [char;5],character: char) -> bool {
 
 
 fn main() {
-    let answer = generate_word();
+    let answer = wordlist_actions::random_word();
+    //println!("answer: {}", answer); //<-- comment out for actual build, useful for debugging
+    let answer_list = generate_wordlist(answer);
     print_manual();
     for i in 1..=5 {
         let guess = guessing(i);
-        let coloring = guess_validation(guess, answer);
+        let coloring = guess_validation(guess, answer_list);
+
         for character in 0..5 {
+            //chose in which color which character should appear
             match coloring[character] {
                 "green" => print!("{}{}", color::Fg(color::Green), guess[character]),
                 "red" => print!("{}{}", color::Fg(color::Red), guess[character]),
                 "black" => print!("{}{}", color::Fg(color::Black), guess[character]),
                 _ => print!("{}{}", color::Fg(color::White), guess[character])
             }
-        }
+        };
+
         print!{"\n{}", color::Fg(color::White)};
         if coloring == ["green", "green", "green", "green", "green"] {
             println!("You guessed correctly! Congrats");
+            thread::sleep(time::Duration::new(10, 0));
             break;
         }
+        if (coloring != ["green", "green", "green", "green", "green"]) & (i == 5) {
+            println!("You couldn't find the solution within 5 guesses. The solution was {}. Better luck next time!", answer);
+        }
     };
+
 }
